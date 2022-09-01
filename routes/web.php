9c9d93e1e848f0d47988c2558e74469cb1236f12
Kervecs <ordonezandres202@gmail.com>
2022-09-01 10:13:10 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaisController;
+use App\Http\Controllers\MonedaController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +24,18 @@ ROute::get('/d',function(){
 });
 
 
+
+Route::get('/pais.index',[PaisController::class,'index'])->name('Pais.index');
+Route::get('/pais.create',[PaisController::class,'create'])->name('Pais.create');
+Route::post('/pais.store',[PaisController::class,'store'])->name('Pais.store');
+Route::get('/editarpais/{id}',[PaisController::class,'edit'])->name('editarpregunta');
+Route::put('/actualizarpais/{id}',[PaisController::class,'update'])->name('actualizarpais');
+Route::delete('/eliminarpais/{id}',[PaisController::class,'destroy'])->name('eliminarpais');
+
+
+Route::get('/moneda.index',[MonedaController::class,'index'])->name('moneda.index');
+Route::get('/moneda.create',[MonedaController::class,'create'])->name('moneda.create');
+Route::post('/moneda.store',[MonedaController::class,'store'])->name('moneda.store');
+Route::get('/editarmoneda/{id}',[MonedaController::class,'edit'])->name('editarmoneda');
+Route::put('/actualizarmoneda/{id}',[MonedaController::class,'update'])->name('actualizarmoneda');
+Route::delete('/eliminarmoneda/{id}',[MonedaController::class,'destroy'])->name('eliminarmoneda');
