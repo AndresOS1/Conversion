8fb0f416e08d9757ca53f432d1f39ba1be330a66
Kervecs <ordonezandres202@gmail.com>
2022-09-01 17:06:50 -0500

    Funcionalidad

--- public/js/logica.js
+++ public/js/logica.js
@@ -1,37 +1,23 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
     $('#select-pais').on('change', valorMoneda);
-
-    
 });
 
-let valorMoneda = 0;
+let valueGlobal = 0
 
-function getValueInput() {
-    let valor = document.getElementById("cantidad").value; 
-    // document.getElementById("valueInput").innerHTML = inputValue; 
-    alert(valor)
-
-
+function getValueInput() {       
+    let valor = document.getElementById("cantidad").value;
+    let resultado = valor * valueGlobal;    
+    document.getElementById("resultado").innerHTML = resultado;    
 }
 
-function valorMoneda(){
-
+function valorMoneda() {
     let id_moneda = $(this).val();
-    alert(id_moneda);
- 
-
-    $.get('/api/moneda/'+id_moneda+'', function(data){
-
-        for(let i=0; i<data.length; i++){
-
+    var valorMonedaUsd = 0
+    $.get('/api/moneda/' + id_moneda + '', function (data) {
+        for (let i = 0; i < data.length; i++) {
             console.log(data[i]);
-            // valorMoneda = data[i].valor_usd;
-            // console.log(valorMoneda);
+            valorMonedaUsd = data[i].valor_usd;
         }
-        
+        valueGlobal= valorMonedaUsd       
     })
 }
-  
-
-
