553f4da0072d93a9359a03d80a27abb8cc24113f
Kervecs <ordonezandres202@gmail.com>
2022-09-01 10:48:37 -0500

    Corrgiendo Modelos

--- app/Models/Moneda.php
+++ app/Models/Moneda.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Moneda extends Model
 {
     use HasFactory;
+
+    protected $table  = "monedas";
+
+    protected $primaryKey = "id_moneda";
+
+
+    protected $fillable = ['moneda','valor','valor_usd'];
+
+    protected $hidden = ['id_moneda'];
 }
